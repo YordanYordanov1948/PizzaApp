81f51bd36606dcd2b7a3d7647a945bc71a87eb37
Yordan Yordanov <46786238+YordanYordanov1948@users.noreply.github.com>
2020-12-21 15:13:10 +0200

    Nav bar update

--- src/components/Navigation/Toolbar.js
+++ src/components/Navigation/Toolbar.js
@@ -4,24 +4,59 @@ import styled from 'styled-components';
 const toolbar = (props) => {
     return (
 <Toolbar>
+<Li>
+    <Anchor>
+      Home
+    </Anchor>
 
-    Menu
-    
-  
+    <Anchor>
+      About
+    </Anchor>
+
+    <Anchor>
+      Pricing
+    </Anchor>
+
+    <Anchor>
+     Menu
+    </Anchor>
+
+    <Anchor>
+     Contact us
+    </Anchor> 
+</Li>
 </Toolbar>
     );
-
 }
 
 export default toolbar;
 
 
 const Toolbar = styled.div`
-overflow: hidden;
-background-color: #FFFF00;
-position: fixed;
-top: 0; 
-width: 99%;
-font-size: 25px;
+background-color: #333;
+position: fixed; 
+top: 0;
+width: 100%;
+font-family:"Dosis", sans-serif; 
 `;
+
+const Li = styled.div`
+display:flex;
+justify-content: center;
+text-decoration:none;
+text-transform:uppercase;
+`;
+
+const Anchor = styled.a`
+display:flex;
+text-decoration:none;
+color:#FFFF00;
+padding:1rem;
+
+&:hover {
+    opacity: 0.7;
+
+  }
+  `;
+
 
